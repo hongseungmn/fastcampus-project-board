c3434ba707f031f2b1924c3d1e5e013fd54362bc
홍성민 <79087697+hongseungmn@users.noreply.github.com>
2023-06-04 16:56:22 +0900

    비즈니스 로직 구현
    테스트를 토대로 게시판 서비스에 필요한 비즈니스 로직 구현 완료

--- board-project/src/test/java/com/example/boardproject/controller/ArticleControllerTest.java
+++ board-project/src/test/java/com/example/boardproject/controller/ArticleControllerTest.java
@@ -1,16 +1,31 @@
 package com.example.boardproject.controller;
 
 import com.example.boardproject.config.SecurityConfig;
+import com.example.boardproject.dto.ArticleWithCommentsDto;
+import com.example.boardproject.dto.UserAccountDto;
+import com.example.boardproject.service.ArticleService;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -20,7 +35,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class ArticleControllerTest {
 
   private final MockMvc mvc;
-
+  @MockBean private ArticleService articleService;
   public ArticleControllerTest(@Autowired MockMvc mvc) {
     this.mvc = mvc;
   }//테스트패키지 안에서는 AutoWired 생략이 불가능하다
@@ -30,28 +45,31 @@ class ArticleControllerTest {
   @Test
   public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
     //Given
-
+    given(articleService.searchArticles(eq(null),eq(null),any(Pageable.class))).willReturn(Page.empty());
     // When & Then
     mvc.perform(get("/articles"))
             .andExpect(status().isOk())//200ok가 되었는가
             .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))//contentType이 올바른가 contentTypeCompatibleWith는 동일 계열이 맞으면 통과한다
             .andExpect(view().name("articles/index"))//실제 배치될 뷰의 이름 테스트(해당 이름의 뷰 존재 여부 파악)
             .andExpect(model().attributeExists("articles"));
+    then(articleService).should().searchArticles(eq(null),eq(null),any(Pageable.class));
   }
 
-  @Disabled("구현 중")
+  //@Disabled("구현 중")
   @DisplayName("[view] [GET] 게시글 상세 페이지 - 정상호출")
   @Test
   public void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
     //Given
-
+    Long articleId = 1L;
+    given(articleService.getArticle(articleId)).willReturn(createArticleWithCommentsDto());
     // When & Then
-    mvc.perform(get("/articles/1"))
+    mvc.perform(get("/articles/"+articleId))
             .andExpect(status().isOk())
             .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
             .andExpect(view().name("articles/detail"))
             .andExpect(model().attributeExists("article"))
             .andExpect(model().attributeExists("articleComments"));
+    then(articleService).should().getArticle(articleId);
   }
 
   @Disabled("구현 중")
@@ -78,6 +96,37 @@ class ArticleControllerTest {
             .andExpect(status().isOk())
             .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
             .andExpect(view().name("articles/search-hashtag"));
+  }
+
+  private ArticleWithCommentsDto createArticleWithCommentsDto() {
+    return ArticleWithCommentsDto.of(
+            1L,
+            createUserAccountDto(),
+            Set.of(),
+            "title",
+            "content",
+            "#java",
+            LocalDateTime.now(),
+            "uno",
+            LocalDateTime.now(),
+            "uno"
+    );
+  }
+
+  private UserAccountDto createUserAccountDto() {
+    return UserAccountDto.of(
+            1L,
+            "uno",
+            "pw",
+            "uno@mail.com",
+            "Uno",
+            "memo",
+            LocalDateTime.now(),
+            "uno",
+            LocalDateTime.now(),
+            "uno"
+    );
+
   }
 
 }
